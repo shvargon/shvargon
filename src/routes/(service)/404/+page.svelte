f1e77b007794b76a73e264fbae2fc0f0dba13b3d
Valery Shvargonov <animekazna@gmail.com>
2025-02-05 10:09:26 +0000

    404 page?

---
+++ src/routes/(service)/404/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col h-full items-center justify-center">
+    <h1>Произошла ошибка.</h1>
+    <span>Попробуйте позже или <a href="/">вернитесь на главную страницу</a></span>
+</div>
