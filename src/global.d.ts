ef7e9a0a31bb765855d379597497c8d2877ecbf9
Valery Shvargonov <animekazna@gmail.com>
2025-02-05 05:39:35 +0000

    оптимизация md файлов регистрация typescript типа и настройка иконки vscode

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+declare module '*.svx' {
+	import type { SvelteComponent } from 'svelte';
+
+	export default class Comp extends SvelteComponent {};
+	export const metadata: Record<string, unknown>;
+}
