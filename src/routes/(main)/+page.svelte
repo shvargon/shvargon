96ebe31f95a4f72c1c14a706519d92cfa863995a
Valery Shvargonov <animekazna@gmail.com>
2025-02-05 11:22:58 +0000

    main page description and title

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,1 +1,6 @@
+<svelte:head>
+  <title>Shvargon IT, обучение и программирование</title>
+  <meta name="description" content="Блог о мире IT: от основ работы с ПК до администрирования и программирования. Полезные советы и понятные объяснения. Автор: shvargon." />
+</svelte:head>
+
 <h1 class="text-blue-400">Hello world</h1>
