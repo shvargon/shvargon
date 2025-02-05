c4d7ecaf0f72e673e8531e78793e4a686ec31c2e
Valery Shvargonov <animekazna@gmail.com>
2025-02-05 05:35:19 +0000

    добавление поддержки markdown страниц

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: [".svelte", ".svx"],
+	
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		mdsvex({ extensions: ['.svx', '.md'] }), 
+		vitePreprocess()
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
