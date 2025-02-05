30dfda2bf85e73fc80a469ee90b57aef2a3d533c
Valery Shvargonov <animekazna@gmail.com>
2025-02-05 13:36:07 +0000

    исправляем дальше калькулятор сети

--- src/routes/(tools)/tools/network-calculator/+page.svelte
+++ src/routes/(tools)/tools/network-calculator/+page.svelte
@@ -1,9 +1,9 @@
 <script>
     let ipAddress = $state([192, 168, 0, 1]); // Изначальный IP-адрес
     let subnetMask = $state(24); // Изначальная маска подсети
-    let networkAddress = "";
-    let broadcastAddress = "";
-    let availableHosts = 0;
+    let networkAddress = $state("");
+    let broadcastAddress = $state("");
+    let availableHosts = $state(0);
 
     const getSubnetMask = (cidr) => {
         let mask = "";
@@ -48,44 +48,54 @@
     });
 </script>
 
+<h1 class="font-semibold text-center">Калькулятор сети</h1>
 <div class="max-w-4xl mx-auto p-6 space-y-4 bg-white rounded-lg shadow-lg">
-    <h2 class="text-2xl font-semibold text-center">Калькулятор сети</h2>
-
+    
+    <h2>IpV4 адрес</h2>
     <!-- Ввод IP-адреса -->
     <div class="flex space-x-2">
+        
         <input
             type="number"
             bind:value={ipAddress[0]}
             min="0"
             max="255"
-            class="input w-1/4"
-        />
+            class="input w-1/5"
+        />.
         <input
             type="number"
             bind:value={ipAddress[1]}
             min="0"
             max="255"
-            class="input w-1/4"
-        />
+            class="input w-1/5"
+        />.
         <input
             type="number"
             bind:value={ipAddress[2]}
             min="0"
             max="255"
-            class="input w-1/4"
-        />
+            class="input w-1/5"
+        />.
         <input
             type="number"
             bind:value={ipAddress[3]}
             min="0"
             max="255"
-            class="input w-1/4"
+            class="input w-1/5"
+        />
+        /
+        <input
+            type="number"
+            bind:value={subnetMask}
+            min="0"
+            max="255"
+            class="input w-1/5"
         />
     </div>
 
     <!-- Выбор маски подсети -->
     <div>
-        <label class="block">Маска подсети</label>
+        <h2 class="block">Маска подсети</h2>
         <div class="flex flex-col">
             <select bind:value={subnetMask} class="select select-bordered">
                 {#each subnetMasks as mask}
